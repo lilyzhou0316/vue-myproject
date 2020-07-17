cf3cf8ce1bc8c17f9138cc513df01cffd35c4619
lilyzhou0316 <lilyzhou0316@hotmail.com>
2020-07-17 16:56:31 -0700

    request data from api

---
+++ src/network/home.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-17 16:27:39
+ * @LastEditTime: 2020-07-17 16:33:57
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue/shopping-app/src/network/home.js
+ */
+import {
+  request
+} from './request';
+
+// 因为首页一定会不止一个数据请求，所以需要在这里对所有的请求进行一个管理
+//方便后续进行修改，而不是写到home.vue里
+export function getHomeMultiData() {
+  return request({ //返回的是一个promise对象
+    url: '/home/multidata'
+  })
+
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-15 18:12:06
- * @LastEditTime: 2020-07-17 16:19:05
+ * @LastEditTime: 2020-07-17 16:55:32
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue/my-project/src/views/home/Home.vue
@@ -16,11 +16,28 @@
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-
+import { getHomeMultiData } from "network/home";
 export default {
   name: "Home",
   components: {
     NavBar
+  },
+  data() {
+    return {
+      banners: [], //保存从数据库里拿到的数据数组
+      recommends: []
+    };
+  },
+  created() {
+    //组件创建完成就会调用created方法
+    //所以需要在这里发送home页的数据请求
+    getHomeMultiData()
+      .then(res => {
+        //console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+      .catch();
   }
 };
 </script>
