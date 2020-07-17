930eb2b88a956e10c587e54d4872f98e5d999193
lilyzhou0316 <lilyzhou0316@hotmail.com>
2020-07-17 16:19:47 -0700

    home nav-bar created

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,45 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-07-17 15:51:32
+ * @LastEditTime: 2020-07-17 16:16:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue/shopping-app/src/components/common/navbar/NavBar.vue
+-->
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100, 100, 100, 0.1);
+}
+
+.left,
+.right {
+  width: 60px;
+  /* 测试用：background-color: pink; */
+}
+
+.center {
+  flex: 1;
+  /* background-color: pink; */
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,22 +1,34 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-15 18:12:06
- * @LastEditTime: 2020-07-15 18:14:34
+ * @LastEditTime: 2020-07-17 16:19:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue/my-project/src/views/home/Home.vue
 -->
 <template>
-  <div id="">
-    home
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    NavBar
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.home-nav {
+  background-color: pink;
+  color: azure;
+  font-size: 20px;
+}
+</style>
