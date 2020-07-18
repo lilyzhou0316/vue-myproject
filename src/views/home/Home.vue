a0bcd5a750f5481ac6417cd70d736a9a09c67a37
lilyzhou0316 <lilyzhou0316@hotmail.com>
2020-07-17 17:59:30 -0700

    home主页轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-15 18:12:06
- * @LastEditTime: 2020-07-17 16:55:32
+ * @LastEditTime: 2020-07-17 17:57:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue/my-project/src/views/home/Home.vue
@@ -11,16 +11,19 @@
     <nav-bar class="home-nav">
       <div slot="center">购物车</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
 import { getHomeMultiData } from "network/home";
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper
   },
   data() {
     return {
@@ -33,7 +36,7 @@ export default {
     //所以需要在这里发送home页的数据请求
     getHomeMultiData()
       .then(res => {
-        //console.log(res);
+        console.log(res);
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
       })
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-07-17 17:50:49
+ * @LastEditTime: 2020-07-17 17:56:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue/shopping-app/src/views/home/childComps/HomeSwiper.vue
+-->
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" :key="item.index">
+      <a :href="item.link">
+        <img :src="item.image" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper/index";
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {};
+  },
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return[];
+      }
+    },
+    recommends: Array
+  },
+  components: { Swiper, SwiperItem }
+};
+</script>
+
+<style scoped></style>
