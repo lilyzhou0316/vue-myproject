96420bcf843e2d314d9a3b6cd53008042a163de3
lilyzhou0316 <lilyzhou0316@hotmail.com>
2020-07-17 21:28:02 -0700

    fasion view created

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-15 18:12:06
- * @LastEditTime: 2020-07-17 20:14:32
+ * @LastEditTime: 2020-07-17 21:27:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue/my-project/src/views/home/Home.vue
@@ -13,6 +13,19 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend :recommends="recommends"></home-recommend>
+    <home-fasion></home-fasion>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
   </div>
 </template>
 
@@ -20,13 +33,15 @@
 import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import HomeRecommend from "./childComps/HomeRecommend";
+import HomeFasion from "./childComps/HomeFasion";
 import { getHomeMultiData } from "network/home";
 export default {
   name: "Home",
   components: {
     NavBar,
     HomeSwiper,
-    HomeRecommend
+    HomeRecommend,
+    HomeFasion
   },
   data() {
     return {
@@ -37,21 +52,34 @@ export default {
   created() {
     //组件创建完成就会调用created方法
     //所以需要在这里发送home页的数据请求
-    getHomeMultiData()
-      .then(res => {
+    getHomeMultiData().then(
+      res => {
         console.log(res);
         this.banners = res.data.banner.list;
         this.recommends = res.data.recommend.list;
-      })
-      .catch();
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 };
 </script>
 
 <style scoped>
+#home {
+  /* 由于顶部导航条固定所以其它组件需要往下移动44px */
+  padding-top: 44px;
+}
 .home-nav {
   background-color: pink;
   color: azure;
   font-size: 20px;
+  /* 顶部导航条固定 */
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 100;
 }
 </style>
---
+++ src/views/home/childComps/HomeFasion.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-07-17 21:03:52
+ * @LastEditTime: 2020-07-17 21:19:24
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue/shopping-app/src/views/home/childComps/HomeFasion.vue
+-->
+<template>
+  <!-- 此组件处理基本与recommend组件一样 -->
+  <div class="home-fasion">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="~assets/img/home/homefasion.jpg" alt="" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeFasion",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.home-fasion img {
+  width: 100%;
+}
+</style>
